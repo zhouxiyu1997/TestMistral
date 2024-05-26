24920849334f72e959bf73a1fc6d6a44dbf21a73
Moonlit1997 <zhouxiyu1997@gmail.com>
2024-05-26 23:25:17 +0800

    1

--- 1.py
+++ 1.py
@@ -1,1 +1,10 @@
-print("Hello, world!")
+from mistralai.client import MistralClient, ChatMessage
+import numpy as np
+import os
+import requests
+
+response = requests.get('https://raw.githubusercontent.com/run-llama/llama_index/main/docs/docs/examples/data/paul_graham/paul_graham_essay.txt')
+text = response.text
+f = open('essay.txt', 'w')
+f.write(text)
+f.close()
